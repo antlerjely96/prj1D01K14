fe14eb0285cc9cd309a5ba4978d19416b735c438
antlerjely96 <antlerjely96@gmail.com>
2023-05-23 16:48:29 +0700

    add demo search, paging, search + paging

--- demoWebD01K14/demoPHPSection2/classes/index.php
+++ demoWebD01K14/demoPHPSection2/classes/index.php
@@ -22,8 +22,33 @@
         include_once '../layout/header.php';
         //Nhúng file open.php để mở kết nối
         include_once '../connect/open.php';
+        //Khai báo biến search
+        $search = "";
+        //Lấy giá trị được search về với điều kiện $_GET['search'] tồn tại
+        if(isset($_GET['search'])){
+            $search = $_GET['search'];
+        }
+        //Khai báo số bản ghi 1 trang
+        $recordOnePage = 3;
+        //Query để lấy số bản ghi
+        $sqlCountRecord = "SELECT COUNT(*) AS count_record FROM classes WHERE name LIKE '%$search%'";
+        //Chạy query lấy số bản ghi
+        $countRecords = mysqli_query($connect, $sqlCountRecord);
+        //foreach để lấy số bản ghi
+        foreach ($countRecords as $countRecord){
+            $records = $countRecord['count_record'];
+        }
+        //Tính số trang
+        $countPage = ceil($records / $recordOnePage);
+        //Lấy trang hiện tại (nếu không tồn tại page hiện tại thì page hiện tại = 1)
+        $page = 1;
+        if(isset($_GET['page'])){
+            $page = $_GET['page'];
+        }
+        //Tính bản ghi bắt đầu của trang
+        $start = ($page - 1) * $recordOnePage;
         //Query để lấy dữ liệu từ bảng classes trên db về
-        $sql = "SELECT * FROM classes";
+        $sql = "SELECT * FROM classes WHERE name LIKE '%$search%' LIMIT $start, $recordOnePage";
         //Chạy query
         $classes = mysqli_query($connect, $sql);
         //Đóng kết nối
@@ -31,6 +56,12 @@
     ?>
         <a href="../admin/logout.php">Logout</a><br>
         <a href="create.php">Add a class</a>
+<!--        Form để search
+            action để trồng tức là khi click vào button sẽ load lại chính trang này-->
+        <form method="get" action="">
+            Search: <input type="text" name="search" value="<?= $search; ?>"><br>
+            <button>Search</button>
+        </form>
         <table border="1px" cellpadding="0" cellspacing="0" width="100%">
             <tr>
                 <th>ID</th>
@@ -62,6 +93,16 @@
                 }
             ?>
         </table>
+<!--        for để hiển thị số trang-->
+        <?php
+            for($i = 1; $i <= $countPage; $i++){
+        ?>
+                <a href="?page=<?= $i ?>&search=<?= $search ?>">
+                    <?= $i ?>
+                </a>
+        <?php
+            }
+        ?>
     <?php
         //Nhúng footer
         include_once '../layout/footer.php';
